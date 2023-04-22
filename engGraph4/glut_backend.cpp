f873cd4922a4fdc6355026ca3b5e8eab35044049
cowboboy <r2d2tv@yandex.ru>
2023-04-22 16:37:52 +0500

    25

--- engGraph4/glut_backend.cpp
+++ engGraph4/glut_backend.cpp
@@ -1,3 +1,20 @@
+/*
+
+	Copyright 2011 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <stdio.h>
 #include <GL/glew.h>
@@ -5,9 +22,9 @@
 
 #include "glut_backend.h"
 
+// Points to the object implementing the ICallbacks interface which was delivered to
+// GLUTBackendRun(). All events are forwarded to this object.
 static ICallbacks* s_pCallbacks = NULL;
-
-#pragma once
 
 #ifndef __UNISTD_H_17CD2BD1_839A_4E25_97C7_DE9544B8B59C__
 #define __UNISTD_H_17CD2BD1_839A_4E25_97C7_DE9544B8B59C__
@@ -302,27 +319,38 @@ extern "C" {
 
 #endif /* __UNISTD_H_17CD2BD1_839A_4E25_97C7_DE9544B8B59C__ */
 
-static void SpecialKeyboardCB(int Key, int x, int y){
+static void SpecialKeyboardCB(int Key, int x, int y)
+{
     s_pCallbacks->SpecialKeyboardCB(Key, x, y);
 }
 
-static void KeyboardCB(unsigned char Key, int x, int y){
+
+static void KeyboardCB(unsigned char Key, int x, int y)
+{
     s_pCallbacks->KeyboardCB(Key, x, y);
 }
 
-static void PassiveMouseCB(int x, int y){
+
+static void PassiveMouseCB(int x, int y)
+{
     s_pCallbacks->PassiveMouseCB(x, y);
 }
 
-static void RenderSceneCB(){
+
+static void RenderSceneCB()
+{
     s_pCallbacks->RenderSceneCB();
 }
 
-static void IdleCB(){
+
+static void IdleCB()
+{
     s_pCallbacks->IdleCB();
 }
 
-static void InitCallbacks(){
+
+static void InitCallbacks()
+{
     glutDisplayFunc(RenderSceneCB);
     glutIdleFunc(IdleCB);
     glutSpecialFunc(SpecialKeyboardCB);
@@ -330,15 +358,19 @@ static void InitCallbacks(){
     glutKeyboardFunc(KeyboardCB);
 }
 
-void GLUTBackendInit(int argc, char** argv){
+
+void GLUTBackendInit(int argc, char** argv)
+{
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
     glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
 }
 
-bool GLUTBackendCreateWindow(unsigned int Width, unsigned int Height, unsigned int bpp, bool isFullScreen, const char* pTitle){
-    if (isFullScreen){
-        char ModeString[64] = {0};
+
+bool GLUTBackendCreateWindow(unsigned int Width, unsigned int Height, unsigned int bpp, bool isFullScreen, const char* pTitle)
+{
+    if (isFullScreen) {
+        char ModeString[64] = { 0 };
         snprintf(ModeString, sizeof(ModeString), "%dx%d@%d", Width, Height, bpp);
         glutGameModeString(ModeString);
         glutEnterGameMode();
@@ -348,8 +380,9 @@ bool GLUTBackendCreateWindow(unsigned int Width, unsigned int Height, unsigned i
         glutCreateWindow(pTitle);
     }
 
+    // Must be done after glut is initialized!
     GLenum res = glewInit();
-    if (res != GLEW_OK){
+    if (res != GLEW_OK) {
         fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
         return false;
     }
@@ -357,19 +390,18 @@ bool GLUTBackendCreateWindow(unsigned int Width, unsigned int Height, unsigned i
     return true;
 }
 
-void GLUTBackendRun(ICallbacks* pCallbacks){
-    if (!pCallbacks){
+void GLUTBackendRun(ICallbacks* pCallbacks)
+{
+    if (!pCallbacks) {
         fprintf(stderr, "%s : callbacks not specified!\n", __FUNCTION__);
         return;
     }
 
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
-    glEnable(GL_DEPTH_TEST);
-
     glFrontFace(GL_CW);
     glCullFace(GL_BACK);
     glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
 
     s_pCallbacks = pCallbacks;
     InitCallbacks();
